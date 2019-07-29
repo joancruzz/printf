291722dcb4780e4d64d6aa1d41bfb995d59743fa
Andrew C <a.cox@me.com>
2019-07-29 22:39:35 +0000

    Add printf.c file

---
+++ printf.c
@@ -0,0 +1,54 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * printf - prints all datatypes
+ * @format: data type to be printed
+ * Return: length of input
+ */
+int _printf(const char * const format, ...)
+{
+
+	int x;
+	int y;
+	va_list arguments;
+	int counter = 0;
+
+	data_t type[] = {
+/*		{"c", _printchar}, */
+	/*	{"i", _printint}, */
+	/*	{"d", _printdig}, */
+		{"s", _printstr},
+		{NULL, NULL}
+	};
+
+	va_start(arguments, format);
+
+	if ((format[0] == '%' && format[1] == '\0') || format == NULL)
+		return (-1);
+
+	for (x = 0; format != NULL && format[x] != '\0'; x++)
+	{
+		if (format[x] != '%')
+		{
+			counter = counter + _putchar(format[x]);
+			continue;
+		}
+		for (y = 0; type[y].result != NULL; y++)
+		{
+			if (*type[y].result == format[x + 1])
+			{
+				counter = counter + type[y].f(arguments);
+				break;
+
+			}
+		}
+		x++;
+		if (type[y].result == NULL)
+		{
+			counter = counter + _putchar('%');
+			counter = counter + _putchar(format[x]);
+		}
+	}
+	va_end(arguments);
+	return (counter);
+}
